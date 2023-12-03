1a7cbe607b984e45ab3956adb266ee2d4bb4b169
dipeshguptadeveloper <dipeshguptadeveloper@gmail.com>
2023-12-03 11:43:49 +0530

    random meal activity added

--- app/src/main/java/com/dkgtech/foodapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/dkgtech/foodapp/fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.dkgtech.foodapp.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.dkgtech.foodapp.activities.RandomMealActivity
 import com.dkgtech.foodapp.databinding.FragmentHomeBinding
 import com.dkgtech.foodapp.models.random_meal.Meal
 import com.dkgtech.foodapp.viewmodels.HomeViewModel
@@ -32,9 +34,17 @@ class HomeFragment : Fragment() {
 
         homeMVVM.getRandomMeal()
         observeRandomMeal()
+        onRandomMealClick()
 
 
         return binding.root
+    }
+
+    private fun onRandomMealClick() {
+        binding.cardViewRandomImage.setOnClickListener {
+            val intent = Intent(requireContext(), RandomMealActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun observeRandomMeal() {
